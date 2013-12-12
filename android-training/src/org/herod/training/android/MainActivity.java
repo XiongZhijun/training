6e7d9d49529a2246da6842ba2aa91542a80d3158
XiongZhijun <hust.xzj@gmail.com>
2013-12-12 15:33:25 +0800

    演示Menu

--- android-training/src/org/herod/training/android/MainActivity.java
+++ android-training/src/org/herod/training/android/MainActivity.java
@@ -8,13 +8,18 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private static final int CONTEXT_MENU_2 = 2;
+	private static final int CONTEXT_MENU_1 = 1;
 	private BroadcastReceiver receiver;
 
 	@Override
@@ -41,6 +46,8 @@ public class MainActivity extends Activity {
 
 		};
 		registerReceiver(receiver, filter, null, scheduler);
+
+		registerForContextMenu(findViewById(R.id.button8));
 	}
 
 	@Override
@@ -55,6 +62,58 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.login:
+			Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.regist:
+			Toast.makeText(this, "regist", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.help:
+			Toast.makeText(this, "help", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.action_settings:
+			Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.basic_settings:
+			Toast.makeText(this, "basic settings", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.system_settings:
+			Toast.makeText(this, "system settings", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		if (v.getId() != R.id.button8) {
+			return;
+		}
+		menu.add(0, CONTEXT_MENU_1, 0, "菜单1");
+		menu.add(0, CONTEXT_MENU_2, 0, "菜单2");
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case CONTEXT_MENU_1:
+			Toast.makeText(this, "菜单1被选中！", Toast.LENGTH_SHORT).show();
+			return true;
+		case CONTEXT_MENU_2:
+			Toast.makeText(this, "菜单2被选中！", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			break;
+		}
+		return super.onContextItemSelected(item);
 	}
 
 	public void showLayouts(View view) {
@@ -76,9 +135,9 @@ public class MainActivity extends Activity {
 	public void testContentProvider(View v) {
 		startActivity(new Intent(this, ContentProviderActivity.class));
 	}
+
 	public void testAsyncTask(View v) {
 		new SimpleAsyncTask(this).execute(2000L);
 	}
-	
 
 }
