662b044bf0e663c7053e1dbae9b0d9a814e2ede5
XiongZhijun <hust.xzj@gmail.com>
2013-12-20 15:01:07 +0800

    演示PopupWindow

--- android-training/src/org/herod/training/android/GoodsActivity.java
+++ android-training/src/org/herod/training/android/GoodsActivity.java
@@ -3,12 +3,20 @@ package org.herod.training.android;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class GoodsActivity extends Activity {
 
@@ -23,12 +31,48 @@ public class GoodsActivity extends Activity {
 		shopId = getIntent().getLongExtra("shopId", -1);
 		setTitle(shopName);
 		listView = (ListView) findViewById(R.id.listView);
+		listView.setOnItemLongClickListener(new OnGoodsLongClickListener());
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		new GoodsLoadTask(this).execute(shopId);
+	}
+
+	class OnGoodsLongClickListener implements OnItemLongClickListener {
+
+		@Override
+		public boolean onItemLongClick(AdapterView<?> parent, View view,
+				int position, long id) {
+			View contentView = getLayoutInflater().inflate(
+					R.layout.popup_window, null);
+			final PopupWindow popupWindow = new PopupWindow(contentView);
+			popupWindow.setWidth(LayoutParams.WRAP_CONTENT);
+			popupWindow.setHeight(LayoutParams.WRAP_CONTENT);
+			contentView.findViewById(R.id.edit).setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							Toast.makeText(getApplicationContext(), "编辑商品",
+									Toast.LENGTH_SHORT).show();
+							popupWindow.dismiss();
+						}
+					});
+			contentView.findViewById(R.id.delete).setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							Toast.makeText(getApplicationContext(), "编辑商品",
+									Toast.LENGTH_SHORT).show();
+							popupWindow.dismiss();
+						}
+					});
+			popupWindow.setFocusable(true);
+			popupWindow.setOutsideTouchable(true);
+			popupWindow.setBackgroundDrawable(new BitmapDrawable());
+			popupWindow.showAsDropDown(view, 100, -50);
+			return true;
+		}
+
 	}
 
 	class GoodsLoadTask extends AsyncTask<Long, Void, Cursor> {
